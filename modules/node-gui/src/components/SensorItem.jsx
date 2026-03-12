8da8414551ec1a4e48d39347a3469b9285e75567
Awul <aron.schliep@akustik.rwth-aachen.de>
2026-03-12 18:11:37 +0100

    Added units to node_gui

--- modules/node-gui/src/components/SensorItem.jsx
+++ modules/node-gui/src/components/SensorItem.jsx
@@ -4,50 +4,57 @@ import {
   WiDaySunny,
   WiStrongWind,
 } from "react-icons/wi";
-import {
-  FaQuestion,
-} from "react-icons/fa";
+import { FaQuestion } from "react-icons/fa";
 import { MdAudiotrack, MdAir, MdCloud } from "react-icons/md";
 
 export default function SensorItem({ name, value }) {
   let Icon = null;
   let colorClass = "text-gray-300";
+  let unit = ""; // <-- new
 
   switch (name.toLowerCase()) {
     case "temperature":
     case "temp":
       Icon = WiThermometer;
       colorClass = "text-red-400";
+      unit = "°C";
       break;
     case "humidity":
     case "hum":
       Icon = WiHumidity;
       colorClass = "text-blue-400";
+      unit = "%";
       break;
     case "light":
       Icon = WiDaySunny;
       colorClass = "text-yellow-400";
+      unit = "lx"; // lux
       break;
     case "audio":
       Icon = MdAudiotrack;
       colorClass = "text-purple-400";
+      unit = "dB"; // decibels
       break;
     case "airquality":
     case "air":
       Icon = MdAir;
       colorClass = "text-gray-200";
+      unit = "AQI"; // Air Quality Index
       break;
     case "co2":
       Icon = MdCloud;
       colorClass = "text-gray-400";
+      unit = "ppm";
       break;
     case "pressure":
       Icon = WiStrongWind;
       colorClass = "text-indigo-400";
+      unit = "hPa";
       break;
     default:
       Icon = FaQuestion;
       colorClass = "text-gray-300";
+      unit = "";
   }
 
   return (
@@ -56,7 +63,7 @@ export default function SensorItem({ name, value }) {
         {Icon && <Icon className={`w-4 h-4 ${colorClass}`} />}
         <span>{name}</span>
       </div>
-      <span>{value}</span>
+      <span>{value !== null && value !== undefined ? `${value} ${unit}` : "n/a"}</span>
     </div>
   );
 }
